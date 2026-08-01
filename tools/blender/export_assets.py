993870b5f078a23a54665c1004f43741787cf1d6
syd1hadr <syedsaqlain183@gmail.com>
2026-08-02 00:55:10 +0200

    Add export assets script

---
+++ tools/blender/export_assets.py
@@ -0,0 +1,13 @@
+"""
+Export 3D assets placeholder.
+"""
+
+def main():
+    print("Exporting assets...")
+    print("Export GLB")
+    print("Export FBX")
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
